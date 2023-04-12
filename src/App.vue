79fc50ec4aac81714f19387679d425fb8f81013d
Fedekh <federicocet@gmail.com>
2023-04-12 15:40:42 +0200

    conclude basis exercise

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,14 @@ import AppTitle from "./components/AppTitle.vue";
 export default {
   components: {
     AppTitle
-  },
-  data(){
-    return{
-      title: "La mia prima app con Vite!"
-    }
   }
 }
 </script>
 
 <template>
-<h1>{{ title }}</h1>
+
+<AppTitle/>
+
 </template>
 
 <style scoped>
